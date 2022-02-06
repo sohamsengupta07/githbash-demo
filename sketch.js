3b5a0456a8f09eac295a4dc528e70bfa17d1674b
unknown <soham.sengupta07@gmail.com>
2022-02-06 13:28:11 +0530

    added a text

--- sketch.js
+++ sketch.js
@@ -41,6 +41,7 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+    text("angry bird game ",60,100)
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
